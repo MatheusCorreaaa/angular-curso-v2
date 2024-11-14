59c779c294ccf26f0d28caf0aed28fd17969ba4c
MatheusCorreaaa <mcorreia073@gmail.com>
2024-11-14 05:10:49 -0300

    property-style-event

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss']
+  styleUrls: ['./data-binding.component.scss'],
 })
 export class DataBindingComponent implements OnInit {
+  url: string = 'http://loiane.com';
+  urlImagem = 'https://picsum.photos/seed/picsum/400/200';
 
-  constructor() { }
+  cursoAngular: boolean = true;
 
-  ngOnInit(): void {
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  getValor() {
+    return 1;
   }
 
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('botão clicado');
+  }
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
